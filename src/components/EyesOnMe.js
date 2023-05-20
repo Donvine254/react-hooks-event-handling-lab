0cc3d4009585c40273f7660db48eb5b86d9fb598
donvine mugendi <donvinemugendi@gmail.com>
2023-05-20 04:57:33 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
 // Code EyesOnMe Component Here
+import React from "react";
+import { buttonStyle } from "./Keypad";
+const modifiedButtonStyle={
+    ...buttonStyle,
+    width:'50%',
+    backgroundColor:'green',
+    margin:'5px auto'
+}
+
+function EyesOnMe() {
+  function handleEvent(event) {
+    if (event.type === "blur") {
+      console.log("Hey! Eyes on me!");
+    } else if (event.type === "focus") {
+      console.log("Good!");
+    }
+  }
+  return (
+    <button style={modifiedButtonStyle} onBlur={handleEvent} onFocus={handleEvent}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
